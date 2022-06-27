6a155c3543fd94ed1a32429b9dd40aada7253802
Codetoil <ianthisawesomee@gmail.com>
2022-06-27 16:34:28 -0400

    Improve the code of the mod.

--- src/main/java/com/edgeburnmedia/banscreentest/BanScreenTest.java
+++ src/main/java/com/edgeburnmedia/banscreentest/BanScreenTest.java
@@ -8,14 +8,9 @@ public class BanScreenTest implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("banscreentest");
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/edgeburnmedia/banscreentest/mixin/MinecraftClientMixin.java
+++ src/main/java/com/edgeburnmedia/banscreentest/mixin/MinecraftClientMixin.java
@@ -1,18 +1,23 @@
 package com.edgeburnmedia.banscreentest.mixin;
 
+import com.mojang.authlib.minecraft.BanDetails;
 import net.minecraft.client.MinecraftClient;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.UUID;
 
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
 
 	/**
-	 * @author Edgeburn Media
-	 * @reason Force the ban screen to show
+	 * @author Edgeburn Media, Codetoil
+	 * @reason Trick the game into thinking the game is banned.
 	 */
 	@Overwrite
-	public boolean isMultiplayerBanned() {
-		return true;
+	@Nullable
+	public BanDetails getMultiplayerBanDetails() {
+		return new BanDetails(UUID.randomUUID(), null, null, null);
 	}
 }
